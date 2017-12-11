83aeb5196ec7119e98b0021f440940039c7573df
Jaydeep Singh <jdsingh94@gmail.com>
2017-12-10 18:22:16 -0800

    set up main

---
+++ cis22c_lab6_js/main.cpp
@@ -0,0 +1,18 @@
+/*
+Lab 6 - Makeup Lab
+Hashtables with open addressing
+and quadratic probing
+Written By: Jaydeep Singh
+Professor : Manish Goel
+Date	  : Dec 10, 2017
+*/
+#include <string>
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+
+	system("pause");
+	return 0;
+}
